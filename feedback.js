fbe6965cdf35cfa26dc04199dc7b08e58a2ce993
Ndaziramiyep <patrickofficial30@gmail.com>
2024-03-23 07:46:16 +0200

    Uploading javascript codes

---
+++ feedback.js
@@ -0,0 +1,34 @@
+const ratingEls=document.querySelectorAll(".selection");
+const btnEl=document.getElementById("btn");
+const containerEl=document.querySelector(".data-container");
+
+
+let selectedRating=" ";
+
+ratingEls.forEach((ratingEl)=>{
+   ratingEl.addEventListener("click",
+   (event)=>{
+       removeActive();
+       selectedRating=event.target.innerText;
+       event.target.classList.add("active");
+       });
+});
+
+btnEl.addEventListener("click",()=>{
+ if(selectedRating !==" "){
+containerEl.innerHTML=`
+<strong class="title"> Thank you!</strong>
+<br>
+<br>
+<strong> Feedback Status:<span>${selectedRating}</span></strong>
+<p>we'll use your feedback to improve our customer support.</p>
+
+`;
+ }
+});
+
+function removeActive(){
+ratingEls.forEach((ratingEl)=>{
+   ratingEl.classList.remove("active");
+});
+}
